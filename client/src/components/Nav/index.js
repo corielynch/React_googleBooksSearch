55c1566aa677abbaa7bb19be3b97926d89bf9486
Corie Lynch <Lynchc17@gmail.com>
2020-08-27 19:41:48 -0700

    NavBar component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand" href="/">React Google Books Search</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
